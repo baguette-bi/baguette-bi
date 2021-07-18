999ee16d2159053d20d9df627c0a7df4949e1409
Mikhail Akimov <rovinj.akimov@gmail.com>
2021-07-18 15:54:58 +0200

    fix bug: dates in DataRequest

--- baguette_bi/baguette_bi/core/data_request.py
+++ baguette_bi/baguette_bi/core/data_request.py
@@ -11,7 +11,9 @@ class DataRequest:
     def __init__(self, query: str, parameters: Optional[Dict[str, Any]] = None):
         self.query = query
         self.parameters = parameters if parameters is not None else {}
-        self.id = md5(json.dumps(self.dict(), sort_keys=True).encode()).hexdigest()
+        self.id = md5(
+            json.dumps(self.dict(), sort_keys=True, default=str).encode()
+        ).hexdigest()
 
     def dict(self):
         return {"query": self.query, "parameters": self.parameters}
